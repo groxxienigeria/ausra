21e62ec7ae64242874bc61de1ca2b1f8e6352df7
Oladosu Tayo <tayooladosu9@gmail.com>
2022-01-15 14:48:17 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en" class="csstransitions">
+
+
+<!-- Mirrored from finestdevs.com/demos/folio/contact-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 Dec 2021 15:02:52 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ausra - NFTS built by culture</title>
+    <link rel="shortcut icon" href="./assets/image/favicon1.png" type="image/x-icon">
+    <!-- Bootstrap , fonts & icons  -->
+    <link rel="stylesheet" href="./assets/css/bootstrap.css">
+    <link rel="stylesheet" href="./assets/fonts/icon-font/css/style.css">
+    <link rel="stylesheet" href="./assets/fonts/typography-font/typo-fonts.css">
+    <!-- <link rel="stylesheet" href="fonts/fontawesome-5/css/all.css"> -->
+    <!-- Plugin'stylesheets  -->
+    <link rel="stylesheet" href="./assets/plugins/aos/aos.min.css">
+    <link rel="stylesheet" href="./assets/plugins/animatedModal/animate.css">
+    <!-- Vendor stylesheets  -->
+    <!-- <link rel="stylesheet" href="css/settings.css"> -->
+    
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <!-- Custom stylesheet -->
+    <!-- <link rel="stylesheet" href="css/custom.css"> -->
+  </head>
+  
+<body data-theme="dark">
+  <div id="preloader" class="preloader">
+    <span class="counter">0</span>
+  </div>
+  <main>
+    <div class="site-wrapper overflow-hidden position-relative">
+      <!-- Header Area -->
+      <header class="site-header site-header-menu--right">
+        <div class="container-fluid pr-lg--30 pl-lg--30">
+          <nav class="navbar site-navbar offcanvas-active navbar-expand-lg navbar-light">
+            <!-- Brand Logo-->
+            <div class="brand-logo">
+              <a href="index.html">
+                <img src="./assets/image/logo.png">
+              </a>
+            </div>
+            <div class="collapse navbar-collapse" id="mobile-menu">
+              <div class="navbar-nav mr--10">
+                <ul class="navbar-nav main-menu">
+
+                 
+                  <li class="nav-item dropdown">
+                    <a class="nav-link gr-text-color font-weight-bold modal-trigger gr-text-11 portolio-modal-trigger" href="/">about me.</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link gr-text-color font-weight-bold modal-trigger gr-text-11" href="/contact">contact.</a>
+                  </li>
+                  
+                </ul>
+              </div>
+              <button class="d-block d-lg-none offcanvas-btn-close" type="button" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="true" aria-label="Toggle navigation">
+                <i class="gr-cross-icon"></i>
+              </button>
+            </div>
+            <button class="navbar-toggler btn-close-off-canvas hamburger-icon border-0 border-0" type="button" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="false" aria-label="Toggle navigation">
+              <div class="hamburger hamburger--squeeze js-hamburger">
+                <div class="hamburger-box">
+                  <div class="hamburger-inner"></div>
+                </div>
+              </div>
+            </button>
+          </nav>
+        </div>
+      </header>
+      <div class="contact-block">
+        <div class="container gr-pt-lg-17 gr-pt-10 gr-pb-12">
+          <div class="row">
+            <div class="col-xl-5 d-flex align-items-center order-2 gr-mb-7">
+              <div class="contact-widget-box d-inline-block w-100 border-radius-10 gr-p-8 gr-border-radius-10 gr-bg-blue-opacity-1 gr-ml-10">
+                <div class="single-contact-widget gr-pb-9">
+                  <span class="gr-text-8 d-block gr-color-white">Email me at</span>
+                  <a href="mailto:hello@folio.com" class="gr-text-8 font-weight-bold"><span style="color: #8e909c;">hello@ausra.xyz</span></a>
+                </div>
+                <div class="single-contact-widget">
+                  <span class="gr-text-8 d-block gr-color-white">Call me at</span>
+                  <a href="phone:hello@folio.com" class="gr-text-8 font-weight-bold"><span style="color: #8e909c;">+1-402-4983</span></a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 order-1">
+              <div class="gr-pr-lg-7 gr-pr-xl-15 gr-pb-10">
+                <h2 class="gr-text-3 font-weight-bold gr-pb-5">Contact now</h2>
+                <p class="gr-text-8 gr-heading-color gr-pr-xl-1 gr-pb-2">Have a project or question? Send me a message. I will reply within 48 hours.</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      
+        <footer class="gr-py-md-11 gr-pt-0 gr-pb-11">
+          <div class="container">
+            <div class="text-center">
+              <p class="heading-small text-uppercase">© 2022 All RIght reseved</p>
+            </div>
+          </div>
+        </footer>
+    </div>
+  </main>
+  <!-- Vendor Scripts -->
+  <script src="./assets/js/vendor.min.js"></script>
+  <script src="./assets/plugins/animatedModal/animatedmodal.min.js"></script>
+  <script src="./assets/plugins/isotope/isotope.pkgd.min.js"></script>
+  <script src="./assets/plugins/isotope/packery.pkgd.min.js"></script>
+  <script src="./assets/plugins/isotope/image.loaded.js"></script>
+  <script src="../../../code.createjs.com/1.0.0/preloadjs.min.js"></script>
+  <!-- Activation Script -->
+  <script src="./assets/js/preloader.js"></script>
+  <script src="./assets/js/custom.js"></script>
+</body>
+
+
+<!-- Mirrored from finestdevs.com/demos/folio/contact-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 Dec 2021 15:02:52 GMT -->
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en" class="csstransitions">
+
+
+<!-- Mirrored from finestdevs.com/demos/folio/creative-agency-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 Dec 2021 15:00:13 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Ausra - NFTS built by culture</title>
+  <link rel="shortcut icon" href="./assets//image/favicon1.png" type="image/x-icon">
+  <!-- Bootstrap , fonts & icons  -->
+  <link rel="stylesheet" href="./assets/css/bootstrap.css">
+  <link rel="stylesheet" href="./assets/fonts/icon-font/css/style.css">
+  <link rel="stylesheet" href="./assets/fonts/typography-font/typo-fonts.css">
+  <!-- <link rel="stylesheet" href="fonts/fontawesome-5/css/all.css"> -->
+  <!-- Plugin'stylesheets  -->
+  <link rel="stylesheet" href="./assets/plugins/aos/aos.min.css">
+  <link rel="stylesheet" href="./assets/plugins/animatedModal/animate.css">
+  <!-- Vendor stylesheets  -->
+  <!-- <link rel="stylesheet" href="css/settings.css"> -->
+  
+  <link rel="stylesheet" href="./assets/css/main.css">
+  <!-- Custom stylesheet -->
+  <!-- <link rel="stylesheet" href="css/custom.css"> -->
+</head>
+
+<body data-theme-mode-panel-active data-theme="dark">
+  <div id="preloader" class="preloader">
+    <span class="counter">0</span>
+  </div>
+  <main>
+    <div class="site-wrapper overflow-hidden position-relative">
+      <!-- Header Area -->
+      <header class="site-header site-header-menu--right">
+        <div class="container-fluid pr-lg--30 pl-lg--30">
+          <nav class="navbar site-navbar offcanvas-active navbar-expand-lg navbar-light">
+            <!-- Brand Logo-->
+            <div class="brand-logo">
+              <a href="index.html">
+                <img src="./assets//image/logo.png">
+              </a>
+            </div>
+            <div class="collapse navbar-collapse" id="mobile-menu">
+              <div class="navbar-nav mr--10">
+                <ul class="navbar-nav main-menu">
+
+                 
+                  <li class="nav-item dropdown">
+                    <a class="nav-link gr-text-color font-weight-bold modal-trigger gr-text-11 portolio-modal-trigger" href="/">about me.</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link gr-text-color font-weight-bold modal-trigger gr-text-11" href="/contact">contact.</a>
+                  </li>
+                  
+                </ul>
+              </div>
+              <button class="d-block d-lg-none offcanvas-btn-close" type="button" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="true" aria-label="Toggle navigation">
+                <i class="gr-cross-icon"></i>
+              </button>
+            </div>
+            <button class="navbar-toggler btn-close-off-canvas hamburger-icon border-0 border-0" type="button" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="false" aria-label="Toggle navigation">
+              <div class="hamburger hamburger--squeeze js-hamburger">
+                <div class="hamburger-box">
+                  <div class="hamburger-inner"></div>
+                </div>
+              </div>
+            </button>
+          </nav>
+        </div>
+      </header>
+      <div class="hero-section ">
+        <div class="hero-pattern absolute-top gr-abs-top w-100 z-index-n1">
+          <img src="image/webp/hero-pattern-2.webp" alt="">
+        </div>
+        <div class="container">
+          <div class="row justify-content-center gr-pt-lg-15 gr-pb-lg-15 gr-pt-10 gr-pb-7">
+            <div class="col-lg-5">
+              <img src="./assets//image/png/titanium.png" class=""/>
+            </div>
+            <div class="col-lg-7">
+              <div class="hero-content text-left gr-pt-5 gr-pl-lg-10 gr-pl-0">
+                <h2 class="gr-text-2 font-weight-bold">#AnxietyBy<span style="color: #323338;">Ausra</span> - NFTs built by culture</h2>
+                <span class="gr-text-7 gr-pt-5 gr-pb-7 d-inline-block">We are building a NFT collection filled with passion, emotions, love and pain. There is a real life story or event behind every art we create. You are not buying another art, you are buying an experience.</span>
+                <div class="gr-pt-3">
+                  <a href="https://opensea.io/AusraNFT" class="btn btn-outline goto rounded-pill gr-text-12 gr-btn-border">Explore on Opensea</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+
+        <footer class="gr-py-md-11 gr-pt-0 gr-pb-11">
+          <div class="container">
+            <div class="text-center">
+              <p class="heading-small text-uppercase">© 2022 All RIght reseved</p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  </main>
+  <!-- Vendor Scripts -->
+  <script src="./assets/js/vendor.min.js"></script>
+  <script src="./assets/plugins/animatedModal/animatedmodal.min.js"></script>
+  <script src="./assets/plugins/isotope/isotope.pkgd.min.js"></script>
+  <script src="./assets/plugins/isotope/packery.pkgd.min.js"></script>
+  <script src="./assets/plugins/isotope/image.loaded.js"></script>
+  <script src="../../../code.createjs.com/1.0.0/preloadjs.min.js"></script>
+  <!-- Activation Script -->
+  <script src="./assets/js/preloader.js"></script>
+  <script src="./assets/js/custom.js"></script>
+</body>
+
+
+
+</html>
